99b967f94b5e2219f2decfe223b94abfa6fb8189
wbannick <william.bannick@yougov.com>
2021-03-05 20:26:44 -0500

    visualize_colors

---
+++ R/visualize_colors.R
@@ -0,0 +1,29 @@
+
+
+#' Visualize Colors
+#' @description This function plots supplied colors in a barplot so the user can get a quick sense of what they look like
+#'
+#' @param color_pal a vector of character corresponding to colors to plot
+#'
+#'
+#' @importFrom graphics barplot
+#' @importFrom stats runif
+#' @examples
+#'
+#' visualize_colors(c("dodgerblue", "darkgrey"))
+#'
+#' \dontrun{
+#'
+#' # for those who use viridis
+#' visualize_colors(viridis::viridis(5))
+#'
+#' }
+#'
+#' @return a barplot (graphics version, not ggplot)
+#' @export
+
+
+visualize_colors <- function(color_pal){
+  num_colors <- length(color_pal)
+  return(graphics::barplot(runif(num_colors, min = 3, max =10), col = color_pal))
+}
