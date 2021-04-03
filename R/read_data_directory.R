c2f24ef14976e28dfc3e508ad2f1c0cc3b5ec2bc
wbannick <will.bannick@gmail.com>
2021-04-03 17:36:27 -0400

    added some helpful options to read_dat_directory; should add more examples/tests

--- R/read_data_directory.R
+++ R/read_data_directory.R
@@ -4,8 +4,10 @@
 #'
 #' @param directory_path path to the data directory
 #' @param map_fxn the map_fxn to use; defaults to map_dfr which is ideal for creating a dataframe
-#' @param read_fxn the function used to read in individual files; defaults to read.csv
-#'
+#' @param read_fxn the function used to read in individual files; defaults to readr::read_csv
+#' @param filename_as_var a logical for whether or not the filename should be stored as a variable; defaults to FALSE
+#' this can be helpful if individual data files do not have variables that can be used to distinguish the files from each other
+#' @param exclude_files_regex a regex that can be used indicate which files in a directory to exclude; defaults to NULL in which case all files are read
 #'
 #' @importFrom stringr str_detect str_remove
 #' @importFrom purrr map_dfr
@@ -15,11 +17,11 @@
 #'
 #' @examples
 #' \dontrun{
-#' directory_path <- "Data/PPIC"
+#' directory_path <- "Data"
 #' #to read the data into a data frame
-#' df <- read_data_directory(directory_path, purrr::map_dfr, haven::read_sav)
+#' df <- read_data_directory(directory_path, purrr::map_dfr, haven::read_csv)
 #' #to read the data into list
-#' data_list <- read_data_directory(directory_path, purrr::map, haven::read_sav)
+#' data_list <- read_data_directory(directory_path, purrr::map, haven::read_csv)
 #'
 #' #with defaults you can also just read data from csvs to a dataframe
 #' df <- read_data_directory(directory_path)
@@ -28,26 +30,39 @@
 #' @return data return depends on map_fxn
 #' @export
 
-read_data_directory <- function(directory_path, map_fxn = purrr::map_dfr, read_fxn = readr::read_csv){
-  #if no trailing / let's add it
+read_data_directory <- function(directory_path, map_fxn = purrr::map_dfr, read_fxn = readr::read_csv,
+                                filename_as_var = FALSE, exclude_files_regex = NULL){
+  # if no trailing / let's add it
   if(!stringr::str_detect(directory_path, "/$")){
     directory_path <- paste0(directory_path, "/")
   }
-  #all files
+  # all files
   data_files <- list.files(directory_path)
+  # if they supplied some files to exclude, exclude em
+  if(!(is.null(exclude_files_regex))){
+    data_files <- data_files[!stringr::str_detect(data_files, exclude_files_regex)]
+  }
+  if(length(data_files) == 0){
+    stop("There are no data files to read. Check your directory and exclude_files_regex.")
+  }
   #paste path on
   data_files <- paste0(directory_path, data_files)
 
   #the bulk of the fxn
   return(
     map_fxn(data_files, function(f){
-      #reads file
-      read_fxn(f) %>%
-        #adds file identifier
-        mutate(
-          #grabbing just the file name
-          filename = stringr::str_remove(f, directory_path)
-        )
+      # reads file
+      tmp <- read_fxn(f)
+      # adds the filename if it is desired
+      # (helpful if no var to identify different portions of the stacked data)
+      if(filename_as_var == T){
+        tmp <- tmp %>%
+          mutate(
+            # grabbing just the file name
+            filename = stringr::str_remove(f, directory_path)
+            )
+      }
+      return(tmp)
     })
   )
 
